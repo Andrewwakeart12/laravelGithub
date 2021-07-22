c28548566bb65c9bcbdb14c5974ffbcfa8218b7f
Andrewwakeart12 <andrewwake_art@gmail.com>
2021-07-22 12:25:47 -0400

    estado inicial de la aplicacion

---
+++ config/services.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => "sandbox37d23ddb504a487a8ef5cce627e711d5.mailgun.org",
+        'secret' => "7b394cf7d5aa0abcfddfecc7f02dcab4-e31dc3cc-c1690f28",
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+];
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Whatever</title>
+</head>
+<body>
+    <h1>.lqjese</h1>
+    <p>
+        asd
+    </p>
+</body>
+</html>

8d3c183fe3f482109d3135d397a2f20eea94a962
Andrewwakeart12 <andrewwake_art@gmail.com>
2021-08-18 17:21:45 -0400

    Hay que añadir: -Pagina que se conecte a la api y consiga los usuarios ('usar para eso Auth::user()' desde el modelo') -pagina para crear usuarios -rutas dinamicas en vue -reducir numero de vistas en blade -un directorio raiz ( no admin) en vue -pagina de chats internos -graficas desde el dashboard asi como un visionado general del contenido de la pagina -integrar el framwork de edicion de posts que tenemos descargados

--- resources/js/components/CreateUser.vue
+++ resources/js/components/CreateUser.vue
@@ -1,7 +1,74 @@
 <template>
-    <div class="container-fluid">
-        <h1>Create user</h1>
-    </div>
+<div class="container">
+
+
+
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user" method="POST" >
+
+                                 <div class="form-group">
+                                    <input type="text" class="form-control form-control-user " id="exampleInputEmail"
+                                        placeholder="Username" name='username'  required autocomplete="username">
+
+
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" id="exampleFirstName" name="firstName"
+                                            placeholder="First Name"  required autocomplete="firstName">
+
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control form-control-user " id="exampleLastName"
+                                            placeholder="Last Name" required autocomplete="lastName" name='lastName'>
+
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address" name='email'  required autocomplete="email">
+
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name="password" class="form-control form-control-user "
+                                            id="exampleInputPassword" placeholder="Password" required autocomplete="new-password">
+
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" name="password_confirmation" class="form-control form-control-user"
+                                            id="exampleRepeatPassword" placeholder="Repeat Password" required autocomplete="new-password">
+
+                                    </div>
+                                </div>
+                                <input type="submit" class="btn btn-primary btn-user btn-block" value="Register">
+                                <hr>
+                                <a href="index.html" class="btn btn-google btn-user btn-block">
+                                    <i class="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+</div>
 </template>
 
 <script>
